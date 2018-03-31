bcdcd2e7523fea940d08eb821a175ed3b96e7857
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-03-31 16:23:37 +0000

    2 ways

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const n = parseInt(prompt('Enter the "n" value of Fibonacci number'));
 
-// const result = findFibo(n);
-// document.write(result);
+const result = findFibo(n);
+document.write(result);
 
 // const resultArray = findFiboArray(n);
 // document.write(resultArray);
@@ -9,24 +9,24 @@ const n = parseInt(prompt('Enter the "n" value of Fibonacci number'));
 const resultCycle = findFiboCycle(n);
 document.write(resultCycle);
 
-// function findFibo(n) {
-//     if (n <= 1){
-//         return 1;
-//     }
-//     return findFibo(n-1) + findFibo(n-2);
-// }
+function findFibo(n) {
+    if (n <= 1){
+        return 1;
+    }
+    return ( findFibo(n-1) + findFibo(n-2) );
+}
 
 
 
 function findFiboCycle(n) {
-let fibTwoPrev = 1;
+let fibTwoPrev = 0;
 let fibOnePrev = 1;
 let fibNext = fibOnePrev + fibTwoPrev;
 let i;
-    for ( i = 3; i <= n; i++ ) {
+    for ( i = 2; i <= n; i++ ) {
         fibNext = fibOnePrev + fibTwoPrev;
         fibTwoPrev = fibOnePrev;
         fibOnePrev = fibNext;
     } 
-    return 'Cycle Fibonacci = ' + fibOnePrev + '<br>';
+    return '<br>' + fibNext;
 }
